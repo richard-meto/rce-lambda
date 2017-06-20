2ebbfa0a226bf5f41e383b425797354bbf401437
Richard Everson <richard.everson@metoffice.gov.uk>
2017-06-20 12:04:42 +0100

    change file path

--- src/main/java/uk/gov/metoffice/nswws/dissemination/handler/RichiesEventHandler.java
+++ src/main/java/uk/gov/metoffice/nswws/dissemination/handler/RichiesEventHandler.java
@@ -3,7 +3,7 @@ package uk.gov.metoffice.nswws.dissemination.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.gov.metoffice.nswws.dissemination.domain.events.RichEvent;
+import rich.event;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,7 +15,7 @@ public class RichiesEventHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
         System.out.println("Test lambda invoked OK");
-        RichEvent richEvent = new RichEvent("Richmond", "Everson");
+        event.RichEvent richEvent = new event.RichEvent("Richmond", "Everson");
         System.out.println("Test lambda finished");
         //NswwsWarningsRetrievedEvent n = new NswwsWarningsRetrievedEvent();
         // System.out.println("Test lambda");
